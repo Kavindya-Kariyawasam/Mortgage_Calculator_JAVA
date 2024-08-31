75f76cab24c5c20f527dffc36586a58b0d9f6e4b
Kavindya-Kariyawasam <kavindyakariyawasam01@gmail.com>
2024-08-31 09:34:04 +0530

    Basic calculator functionality implementation

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.text.NumberFormat;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        final byte monthsPerYear = 12;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter principle amount: ");
+        int principleAmount = scanner.nextInt();
+
+        System.out.print("Annual interest rate: ");
+        float annualInterestRate = scanner.nextFloat();
+        float monthlyInterestRatePercentage = annualInterestRate/monthsPerYear/100;
+
+        System.out.print("Period(Years): ");
+        byte period = scanner.nextByte();
+
+        int numberOfPayments = period * monthsPerYear;
+
+        double calculatedTotal = (principleAmount * monthlyInterestRatePercentage * Math.pow((1 + monthlyInterestRatePercentage), numberOfPayments))/(Math.pow((1 + monthlyInterestRatePercentage), numberOfPayments)-1);
+
+        String Mortgage = NumberFormat.getCurrencyInstance().format(calculatedTotal);
+        System.out.println("Mortgage: " + Mortgage);
     }
 }
