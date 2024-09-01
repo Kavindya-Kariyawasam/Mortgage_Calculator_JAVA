d236d655b44f638cfcc762b73831e63c4d591413
Kavindya-Kariyawasam <kavindyakariyawasam01@gmail.com>
2024-09-01 08:01:05 +0530

    Implementation of basic error handling for inputs.

--- src/Main.java
+++ src/Main.java
@@ -5,19 +5,38 @@ public class Main {
     public static void main(String[] args) {
         final byte monthsPerYear = 12;
 
+        int principleAmount = 0, numberOfPayments = 0;
+        float monthlyInterestRatePercentage = 0;
+
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter principle amount: ");
-        int principleAmount = scanner.nextInt();
+        while (true) {
+            System.out.print("Enter principle amount($1K - $1M): ");
+            principleAmount = scanner.nextInt();
+            if (principleAmount>1_000 && principleAmount<1_000_000)
+                break;
+            System.out.println("Enter a value between 1,000 and 1,000,000");
+        }
 
-        System.out.print("Annual interest rate: ");
-        float annualInterestRate = scanner.nextFloat();
-        float monthlyInterestRatePercentage = annualInterestRate/monthsPerYear/100;
+        while (true) {
+            System.out.print("Annual interest rate: ");
+            float annualInterestRate = scanner.nextFloat();
+            if (annualInterestRate<=30 && annualInterestRate>=1) {
+                monthlyInterestRatePercentage = annualInterestRate/monthsPerYear/100;
+                break;
+            }
+            System.out.println("Enter a value between 1 and 30");
+        }
 
-        System.out.print("Period(Years): ");
-        byte period = scanner.nextByte();
-
-        int numberOfPayments = period * monthsPerYear;
+        while (true) {
+            System.out.print("Period(Years): ");
+            byte period = scanner.nextByte();
+            if (period>=1 && period<=30) {
+                numberOfPayments = period * monthsPerYear;
+                break;
+            }
+            System.out.println("Enter a value between 1 and 30");
+        }
 
         double calculatedTotal = (principleAmount * monthlyInterestRatePercentage * Math.pow((1 + monthlyInterestRatePercentage), numberOfPayments))/(Math.pow((1 + monthlyInterestRatePercentage), numberOfPayments)-1);
 
