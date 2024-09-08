6b5ff3a9b2e5e05a0f70f74ef0a379dd5957ae37
Kavindya-Kariyawasam <kavindyakariyawasam01@gmail.com>
2024-09-08 11:11:33 +0530

    Extracting the readNumber method to the 'Console' class.

--- src/com/mortgageCalculator/Main.java
+++ src/com/mortgageCalculator/Main.java
@@ -1,13 +1,17 @@
+package com.mortgageCalculator;
+
+//import com.mortgageCalculator.Console;
+
 import java.text.NumberFormat;
-import java.util.Scanner;
 
 public class Main {
     final static byte monthsPerYear = 12;
+    final static byte PERCENT = 100;
 
     public static void main(String[] args) {
-        int principleAmount = (int) readNumber("Enter principle amount($1K - $1M): ", 1_000, 1_000_000);
-        float annualInterestRate = (float) readNumber("Annual interest rate: ", 1, 30);
-        byte period = (byte) readNumber("Period(Years): ", 1, 30);
+        int principleAmount = (int) Console.readNumber("Enter principle amount($1K - $1M): ", 1_000, 1_000_000);
+        float annualInterestRate = (float) Console.readNumber("Annual interest rate: ", 1, 30);
+        byte period = (byte) Console.readNumber("Period(Years): ", 1, 30);
 
         printMortgage(principleAmount, annualInterestRate, period);
         printPaymentSchedule(period, principleAmount, annualInterestRate);
@@ -27,23 +31,9 @@ public class Main {
         System.out.println("\nMORTGAGE\n--------\nMonthly payments: " + mortgageFormatted);
     }
 
-    public static double readNumber(String prompt, double min, double max) {
-        Scanner scanner = new Scanner(System.in);
-        double value;
-
-        while (true) {
-            System.out.print(prompt);
-            value = scanner.nextDouble();
-            if (value>=min && value<=max)
-                break;
-            System.out.println("Enter a value between " + min + " and " +max);
-        }
-        return value;
-    }
-
     public static double calculateBalance(double principleAmount, float annualInterestRate, byte years, short numberOfPaymentsMade) {
         int numberOfPayments = years * monthsPerYear;
-        float monthlyInterestRatePercentage = annualInterestRate/monthsPerYear/100;
+        float monthlyInterestRatePercentage = annualInterestRate/monthsPerYear/PERCENT;
 
         double balance = principleAmount *
                 (Math.pow(1+monthlyInterestRatePercentage, numberOfPayments)-Math.pow(1+monthlyInterestRatePercentage, numberOfPaymentsMade))/
@@ -53,7 +43,7 @@ public class Main {
 
     public static double calculateMortgage(double principleAmount, float annualInterestRate, byte years) {
         int numberOfPayments = years * monthsPerYear;
-        float monthlyInterestRatePercentage = annualInterestRate/monthsPerYear/100;
+        float monthlyInterestRatePercentage = annualInterestRate/monthsPerYear/PERCENT;
 
         double mortgage =  (principleAmount * monthlyInterestRatePercentage * Math.pow((1 + monthlyInterestRatePercentage), numberOfPayments))/
                 (Math.pow((1 + monthlyInterestRatePercentage), numberOfPayments)-1);
